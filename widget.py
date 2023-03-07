d03c0266fb36970cd950781f6918b1f8410815fc
satansin123 <122982233+satansin123@users.noreply.github.com>
2023-03-07 10:15:45 +0530

    Update widget.py

--- widget.py
+++ widget.py
@@ -13,30 +13,30 @@ from PyQt5 import QtWidgets
 from PyQt5 import QtCore
 
 class states():
-     def dotAndState(self,state,colour):
+     def dotAndState(self,state,colour,height,fontsize):
         self.state_label=QLabel(state)
         if colour == "red":
             self.dot= QLabel("🔴")
         if colour == "green":
             self.dot= QLabel("🟢")
-        self.dot.setStyleSheet("QLabel{color: rgb(255,0,13); ; font: 12pt  'Oswald'; background-color: rgb(30,30,30);}")
-        self.state_label.setStyleSheet("QLabel{color: #f5fcff; font: 12pt  'Oswald';background-color: rgb(31,31,31); }")
+        self.dot.setStyleSheet("QLabel{color: rgb(255,0,13); ; font: %spt  'Oswald'; background-color: rgb(30,30,30);}" % fontsize)
+        self.state_label.setStyleSheet("QLabel{color: #f5fcff; font: %spt  'Oswald';background-color: rgb(30,30,30); }" % fontsize)
 
         self.layout=QGridLayout()
         self.layout.addWidget(self.dot,1,1,1,1)
         self.layout.addWidget(self.state_label,1,2,1,5)
         self.layout_widget = QtWidgets.QWidget()
         self.layout_widget.setLayout(self.layout)
-        self.layout_widget.setFixedHeight(50)
+        self.layout_widget.setFixedHeight(height)
         return self.layout_widget
      
 class namewidget():
-    def nameline(self,name):
+    def nameline(self,name,width,height,fontsize):
         self.name= QLabel(name)
-        self.name.setStyleSheet("QLabel{color: #f5fcff; font: 10pt  'Oswald';background-color: rgb(20,20,20); }")
+        self.name.setStyleSheet("QLabel{color: #f5fcff; font: %spt  'Oswald';background-color: rgb(20,20,20); }" %fontsize)
         self.line= QLabel('  ')
-        self.line.setFixedWidth(7)
-        self.line.setFixedHeight(30)
+        self.line.setFixedWidth(width)
+        self.line.setFixedHeight(height)
         self.line.setStyleSheet("QLabel{background-color: rgb(59, 146, 184)}")
         name_layout = QHBoxLayout()
         name_layout.addWidget(self.line)
@@ -46,10 +46,10 @@ class namewidget():
         return self.name_widget
     
 class button():
-    def buttonfunc(self,name):
+    def buttonfunc(self,name,fontsize):
         self.button_name= QtWidgets.QPushButton()
         self.button_name.setText(name)
-        self.button_name.setStyleSheet("QPushButton{color: #f5fcff; font: 11pt  'Oswald';background-color: rgb(30,30,30); }")
+        self.button_name.setStyleSheet("QPushButton{color: #f5fcff; font: %spt  'Oswald';background-color: rgb(30,30,30); }" % fontsize)
 
         button_layout=QHBoxLayout()
 
