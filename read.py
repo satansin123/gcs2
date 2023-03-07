6b19cd5a8b8beff815e80fe237b7478fc0a7859c
satansin123 <122982233+satansin123@users.noreply.github.com>
2023-03-08 00:22:59 +0530

    Delete read.py

--- read.py
+++
@@ -1,6 +0,0 @@
-def readfromback(filename):
-    with open(filename) as csv :
-        lines = csv.readlines()
-        for line in reversed(lines):
-            array = [int(string) for string in line.rstrip('\n').split(",")]
-            yeild(array)
